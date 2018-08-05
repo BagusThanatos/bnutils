89df122da019e336a667baa7125c860248774c03
Bagus Nurtomo <bagusthanatos@gmail.com>
2018-08-06 06:27:14 +0700

    This is what happens when you have same named files across projects

---
+++ src/getopt.h
@@ -0,0 +1,75 @@
+// Might not need this one line actually.
+// Remember: Unity Build
+#pragma once
+
+//#include <unistd.h> TODO(bagus) : should also rewrite this
+
+int getopt(
+int argc,
+char *argvp[],
+char *optstring);
+
+extern char *optarg; char *optarg = NULL;
+extern int optind, opterr, optopt;
+
+int optind = 1;int opterr=1;
+
+int
+getopt(
+int argc,
+char *argvp[],
+char *optstring){
+    
+    if(optind >= argc){
+        return -1;
+    };
+    char *opt = argvp[optind];
+    if (!opt || *opt != '-'){
+        if (*(opt+1) == '-' && !*(opt+2)){
+            ++optind;
+            return -1;
+        };
+        if (!(opt+1))
+            return -1;
+    };
+    char *optstr = optstring;
+    if(*optstr == ':') return ':';
+    
+    ++opt;
+    while(*optstr != '\0'){
+        if(*optstr == *opt){
+            if (*(optstr+1) == ':') {
+                if(optind + 1 >= argc) {
+                    // error here
+                    return '?';
+                };
+                
+                optarg = argvp[optind+1];
+                ++optind;
+            };
+        };
+        ++optstr;
+    };
+    if(*optstr == '\0') return '?';
+    ++optind;
+    return *opt;;
+};
+
+int getopt_long(
+int argc, 
+char *argv[],
+struct option *Longopts,
+int *Longindex){
+    
+    return 0;
+};
+
+int getopt_long_only(
+int argc,
+char *argv[],
+char *optstring,
+struct option *Longopts,
+int *Longindex){
+    
+    return 0;
+};
